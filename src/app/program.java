211c9e177f9b364fc0eb316d14dfec371f95c867
Freitas <gabrieledsonfreitas@hotmail.com>
2024-09-30 15:14:28 -0300

    Primeira classe: positions

---
+++ src/app/program.java
@@ -0,0 +1,12 @@
+package app;
+
+import tabuleiro.positions;
+
+public class program {
+    public static void main(String[] args)
+
+    {
+        positions pos = new positions (3, 5);
+        System.out.println(pos);
+    }
+}
