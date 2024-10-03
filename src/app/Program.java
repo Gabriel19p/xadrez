1d595908b56e1b13db8144cc0e284d3a97b34cad
Freitas <gabrieledsonfreitas@hotmail.com>
2024-10-03 19:19:02 -0300

    Tratando exceções e limpando tela

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,11 @@
 package app;
 
+import chess.ChassException;
 import chess.ChessPosition;
 import chess.PartidaXadrez;
 import chess.PecaXadrez;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -14,16 +16,27 @@ public class Program {
         PartidaXadrez partidaXadrez = new PartidaXadrez();
 
         while (true) {
-            UI.printBoard(partidaXadrez.getPecas());
-            System.out.println();
-            System.out.print("Origem: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.limparTela();
+                UI.printBoard(partidaXadrez.getPecas());
+                System.out.println();
+                System.out.print("Origem: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Destino: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Destino: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            PecaXadrez pecaCapturada = partidaXadrez.performChessMove(source, target);
+                PecaXadrez pecaCapturada = partidaXadrez.performChessMove(source, target);
+            }
+            catch (ChassException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
